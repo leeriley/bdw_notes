2792f7c1cba523cd24436489a2a1f75a80875441
Lee Riley <lee@leerileydesigns.com>
2012-02-15 17:36:04 -0700

    this is the commit of my scaffolding and generated model

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120207001456) do
+ActiveRecord::Schema.define(:version => 20120215230203) do
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "name"
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.integer  "category_id"
   end
 
 end
